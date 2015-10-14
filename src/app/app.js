60421b10aa3d888e117150a3a1d00ed20f2a4da6
Molly Kent <kent.molly@gmail.com>
2015-10-14 16:02:15 -0700

    Refactor for better routes

--- src/app/app.js
+++ src/app/app.js
@@ -16,64 +16,47 @@ require('angular-route');
       templateUrl: 'views/gists/gist_preview.html',
       controller: 'GistListController'
     })
-    .when('/details/:gist_id', {
+
+    .when('/detail/:g_id', {
       templateUrl: 'views/gist/gist_detail.html',
       controller: 'GistController'
     });
   });
 
   app.service('dataService', function($http, token) {
-  delete $http.defaults.headers.common['X-Requested-With'];
-  this.getGistList = function() {
-      return $http({
-          method: 'GET',
-          url: 'https://api.github.com/users/mollyfish/gists',
-          headers: {'Authorization': 'token ' + part1 + part2}
-      }).then(function(resp, err) {
-        return resp.data;
-      });
-    }
+    delete $http.defaults.headers.common['X-Requested-With'];
+    this.getGistList = function() {
+        return $http({
+            method: 'GET',
+            url: 'https://api.github.com/users/mollyfish/gists',
+            headers: {'Authorization': 'token ' + part1 + part2}
+        }).then(function(resp, err) {
+          return resp.data;
+        });
+      }
 
-  this.getGistData = function (gistId) {
-    return $http({
-      method: 'GET',
-      url: 'https://api.github.com/gists/' + gistId,
-      headers: {'Authorization': 'token ' + part1 + part2}
+    this.getGistData = function (gistId) {
+      return $http({
+        method: 'GET',
+        url: 'https://api.github.com/gists/' + gistId,
+        headers: {'Authorization': 'token ' + part1 + part2}
+      });
+    };
+  });
+
+
+
+  app.controller('GistListController', function($scope, dataService) {
+    $scope.msg = 'GistListController hears you';
+    dataService.getGistList().then(function(dataResponse) {
+      $scope.gist = dataResponse;
     });
-  };
   });
 
   app.controller('GistController', function ($scope, $routeParams, dataService) {
     $scope.msg = 'OHAI!';
-
-    dataService.getGistData($routeParams.gist_id).then(function(dataResponse) {
-      $scope.gist = dataResponse.data;
+    dataService.getGistData($routeParams.g_id).then(function(dataResponse) {
+      $scope.gist = dataResponse.data;      
     });
-  });
-
-  app.controller('GistListController', function($scope, dataService) {
-    dataService.getGistList().then(function(dataResponse) {
-      $scope.msg = 'GistListController hears you';
-      data = dataResponse;
-      console.dir("before for", data);
-      for (var i = 0; i < data.length; i++) {
-        post = {};
-        post.id = data[i].id;
-        post.url = data[i].html_url;
-        post.title = data[i].description;
-        post.date = data[i].updated_at;
-        post.author = data[i].owner.login;
-        post.avatarSrc = data[i].owner.avatar_url;
-        post.comments = parseInt(data[i].comments);
-        post.files = [];
-        for (file in data[i].files) {
-          var filename = data[i].files[file].filename;
-          post.files.push(filename);
-        }
-        posts.push(post);
-      }
-      console.dir(posts);
-    });
-    this.info = posts;
   });
 })();
