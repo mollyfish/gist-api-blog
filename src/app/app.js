a502401b0683b9787819a399f3cc4ba9a39048c1
Molly Kent <kent.molly@gmail.com>
2015-10-15 14:21:18 -0700

    Fix filename behavior in post

--- src/app/app.js
+++ src/app/app.js
@@ -19,12 +19,16 @@ require('angular-route');
       templateUrl: 'views/gist/gist_detail.html',
       controller: 'GistController'
     })
-    .when('/new/:g_id/edit', {
-      templateUrl: 'views/gist/gist_form.html',
+    .when('/detail/:g_id/edit', {
+      templateUrl: 'views/gist/edit_gist_form.html',
       controller: 'GistFormController'
     })
+    .when('/detail/:g_id/delete', {
+      templateUrl: 'views/gists/gist_preview.html',
+      controller: 'GistDeleteController'
+    })
     .when('/new', {
-      templateUrl: 'views/gist/gist_form.html',
+      templateUrl: 'views/gist/new_gist_form.html',
       controller: 'GistFormController'
     });
   });
@@ -49,20 +53,22 @@ require('angular-route');
       });
     };
 
-    this.createNewGist = function() {
-      var res = $http.post('https://api.github.com/gists/', newGist, {
+    this.createNewGist = function(newGist) {
+      return $http.post('https://api.github.com/gists', newGist, {
         headers: { 'Authorization': 'token ' + part1 + part2 }
-      });
-      res.success(function(data, status, headers, config) {
-        $scope.message = data;
-      });
-      res.error(function(data, status, headers, config) {
-        alert( "failure message: " + JSON.stringify({data: data}));
       });
     };
 
-    this.updateGist = function (gistId) {
-      return $http.put('http://localhost:9000/#/detail/' + gistId);
+    this.updateGist = function (newGist, gistId) {
+      return $http.patch('https://api.github.com/gists/' + gistId, newGist, {
+        headers: { 'Authorization': 'token ' + part1 + part2 }
+      });
+    };
+
+    this.deleteGist = function (gistId) {
+      return $http.delete('https://api.github.com/gists/' + gistId, {
+        headers: { 'Authorization': 'token ' + part1 + part2 }
+      });
     };
   });
 
@@ -76,13 +82,23 @@ require('angular-route');
   app.controller('GistController', function ($scope, $routeParams, dataService) {
     $scope.msg = 'OHAI!';
     dataService.getGistData($routeParams.g_id).then(function(dataResponse) {
-      $scope.gist = dataResponse.data;      
+      $scope.gist = dataResponse.data; 
+      console.dir($scope.gist);     
+    });
+  });
+
+  app.controller('GistDeleteController', function ($scope, $routeParams, dataService, $location) {
+    $scope.msg = 'I is here!';
+    dataService.deleteGist($routeParams.g_id).then(function(dataResponse) {
+      $location.path("/");
+      // $scope.gist = dataResponse.data;      
     });
   });
 
   app.controller("GistFormController", ['$scope', "dataService", "$routeParams", "$location", function ($scope, dataService, $routeParams, $location) {
 
-    $scope.save = saveForm;  //'saveForm' is out of scope.
+    $scope.saveNew = saveNewForm;
+    $scope.saveEdits = saveEditsForm;  //'saveForm' is out of scope.
 
     $scope.gist = {};
 
@@ -96,25 +112,62 @@ require('angular-route');
       }
     }
 
-    function saveForm () {
-      console.log('saveForm has fired');
-      
+    function saveNewForm () {
+      console.log('saveNew has fired');
       $scope.gist.description = document.getElementById('description').value;
-      $scope.gist.fileName = document.getElementById('filename').value;
-      $scope.gist.fileContent = document.getElementById('content').value;
-      newGist = { "description": $scope.gist.description,
+      var filename = document.getElementById('filename').value;
+      var content1 = document.getElementById('content1').value;
+      $scope.gist.files = {};
+      $scope.gist.files[filename] = {content: content1, filename: filename + '.txt'}; 
+      
+      newGist = {
+        "description": $scope.gist.description,
         "public": true,
-        "files": {
-          "newfile-1.txt": {
-            "filename": $scope.gist.fileName,
-            "content": $scope.gist.fileContent
-          }
-        }
+        "files": $scope.gist.files
       };
-      var method = $routeParams.g_id ? "updateGist(g_id)" : "createNewGist";
-      dataService[method]($scope.gist).then(function (resp) {
-        $location.path("/gists/" + resp.data._id); //Bad property name '_id'.
+      var method = $routeParams.g_id ? "updateGist" : "createNewGist";
+      dataService[method](newGist, $routeParams.g_id).then(function (resp) {
+        $location.path("/detail/" + resp.data.id); //Bad property name '_id'.
+        console.log(resp.data.id);
+        console.log(resp);
       });
     }
+
+    function saveEditsForm () {
+      console.log('saveEdits has fired');
+
+
+      $scope.gist.description = document.getElementById('description').value;
+      $scope.gist.updatedContent = document.getElementById('updContent').value;
+      $scope.gist.newFilename = document.getElementById('filename').value;
+      $scope.gist.modContent = document.getElementById('modContent').value;
+      $scope.gist.newContent = document.getElementById('newContent').value;
+      // newGist = { 
+      //   "description": $scope.gist.description,
+      //   "files": {
+
+      //     // "file1.txt": {
+      //     //   "content": $scope.gist.updatedContent },
+
+      //     // "old_name.txt": {
+      //     //   "filename": $scope.gist.newFilename,
+      //     //   "content": $scope.gist.modContent
+      //     // },
+
+      //     // "new_file.txt": {
+      //     //   "content": $scope.gist.newContent
+      //     // },
+
+      //     "delete_this_file.txt": null
+      //   }
+      // };
+      var method = $routeParams.g_id ? "updateGist" : "createNewGist";
+      dataService[method]($routeParams.g_id).then(function (resp) {
+        $location.path("/gists/" + $routeParams.g_id);
+        // console.log(resp.data.id);
+        console.log(resp);
+      });
+    }
+
   }]);
 })();
