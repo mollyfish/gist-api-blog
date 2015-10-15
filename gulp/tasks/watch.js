e0a71560b0f3482333946fa03bb1958bf69eb052
Molly Kent <kent.molly@gmail.com>
2015-10-14 20:31:55 -0700

    Style gist detail page, add content

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,6 +3,6 @@ var config   = require('../config');
 
 gulp.task('watch', function() {
   gulp.watch(config.javascript.src, ['webpack']);
-  gulp.watch(config.sass.src,   ['sass']);
+  gulp.watch(config.sass.src, ['sass']);
   gulp.watch(config.html.src, ['html']);
 });
